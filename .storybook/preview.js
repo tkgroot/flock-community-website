f2f2bc6379da81da83dd67734869fce8bd6f7ae2
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-04-22 20:51:38 +0200

    feat: add storybook
    - add storybook run-script
    - install packages
    - install addons
    - add empty webpack config
    - add head template
    - add custom head to storybooks
    - create footer story
    - add storybook run-script params
    - create gloabal parameters
    - install storybook for react
    - add sass support
    - remove bootstrap stylesheet import
    - add sass-loader dependencies
    - add storybook-static directory to gitignore
    - install storybook-addon-knobs
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import {addParameters} from "@storybook/react"
+
+addParameters({
+  backgrounds: [
+    {name: "white", value: "#fff", default: true},
+    {name: "brown", value: "#383302"},
+    {name: "yellow", value: "#F8E008"},
+    {name: "dark-brown-black", value: "#201D1D"},
+    {name: "light-cotta", value: "#AD9E9E"},
+    {name: "light-brown", value: "#6E6464"},
+  ],
+})
