a63399ab399006d270ab6856540e3d3331554c9f
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-11-29 15:14:45 +0100

    feat: add more flock people
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/content/community.js
+++ src/content/community.js
@@ -3,13 +3,23 @@
  */
 export const peoples = [
   {
-  firstname: "Jerre",
-  lastname: "van Veluw",
-  image: "jerre-hs.jpg"
+    firstname: "Jerre",
+    lastname: "van Veluw",
+    image: "jerre-hs.jpg",
   },
   {
-  firstname: "Vincent",
-  lastname: "de Bruijn",
-  image: "vincent-hs.jpg"
-  }
+    firstname: "Vincent",
+    lastname: "de Bruijn",
+    image: "vincent-hs.jpg",
+  },
+  {
+    firstname: "Niels",
+    lastname: "Simonides",
+    image: undefined,
+  },
+  {
+    firstname: "Casper",
+    lastname: "Peulen",
+    image: undefined,
+  },
 ]
