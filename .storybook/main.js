c7270c13890a547de351a51770770e7237e1d7d8
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2020-11-30 17:57:30 +0100

    !update storybook config
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,10 @@
-const path = require("path")
-
 module.exports = {
-  addons: [
-    "@storybook/addon-a11y/register",
-    "@storybook/addon-actions/register",
-    "@storybook/addon-backgrounds/register",
-    "@storybook/addon-docs/register",
-    "@storybook/addon-knobs/register",
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  stories: ["../src/**/*.stories.[tj]s"],
-  webpackFinal: async (config, {configType}) => {
-     config.module.rules.push({
-       test: /\.s?[ac]ss$/,
-       use: ["style-loader", "css-loader", "sass-loader"],
-       include: path.resolve(__dirname, "../"),
-     })
-
-    return config
-  },
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,4 @@
-import {addParameters} from "@storybook/react"
-import "../src/styles.scss"
 
-addParameters({
-  backgrounds: [
-    {name: "white", value: "#fff", default: true},
-    {name: "brown", value: "#383302"},
-    {name: "yellow", value: "#F8E008"},
-    {name: "dark-brown-black", value: "#201D1D"},
-    {name: "light-cotta", value: "#AD9E9E"},
-    {name: "light-brown", value: "#6E6464"},
-  ],
-})
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
